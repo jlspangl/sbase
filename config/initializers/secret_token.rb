cb27d06ba0657765289a7fe7dd1082a3afd5792f
jlspangl <jan.l.spangler@nasa.gov>
2013-02-26 17:19:16 -0500

    git commit -m "Initial commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbase::Application.config.secret_token = '1c86c756ef63600f5cc4b80abd12128bf0f17576f735f81385fb5adbeea2fa694bc9b0f8a51ddc1bd93176e1a65b674c59dd7d6246c903ffd4cf0507d9c63ac4'
