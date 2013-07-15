efe61f54d9b7bb4fa71e20a23b63ac23e6368741
Jan L. Spangler <jan.l.spangler@nasa.gov>
2013-07-15 15:55:53 -0400

    modify email config for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Sbase::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # mailer used by devise
+
+  config.assets.debug = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'datalab.larc.nasa.gov/sbase' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.larc.nasa.gov",
+      :port => 25,
+      :domain => "larc.nasa.gov",
+      :authentication => nil
 end
